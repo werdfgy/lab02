dca69764bbb89e934c5bd2494c8675987c23057c
werdfgy <Vovazhd2007@gmail.com>
2026-03-18 19:54:51 +0300

    add comments to hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,11 +2,14 @@
 #include <string>
 
 int main() {
+    // Переменная для хранения имени пользователя
     std::string name;
 
+    // Запрос имени у пользователя
     std::cout << "Enter your name: ";
     std::cin >> name;
 
+    // Вывод приветствия с именем
     std::cout << "Hello world from " << name << std::endl;
 
     return 0;
