66bd9654e9a60446f3018c1d9bfae9ecd5ad1418
werdfgy <Vovazhd2007@gmail.com>
2026-03-18 19:40:25 +0300

    fix code style in patch1

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,8 +3,11 @@
 
 int main() {
     std::string name;
+
     std::cout << "Enter your name: ";
     std::cin >> name;
+
     std::cout << "Hello world from " << name << std::endl;
+
     return 0;
 }
