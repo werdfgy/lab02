e8b6c91bee2e321373d4f8601715755b2392d64b
werdfgy <Vovazhd2007@gmail.com>
2026-03-18 18:45:45 +0300

    add hello_world with bad code style

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string name;
+cout<<"Enter your name: ";
+cin>>name;
+cout<<"Hello world from "<<name<<endl;
+return 0;
+}
